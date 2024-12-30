5d29d6ac03c649f68fef5fad48e3a90d9ac1b6d1
SeptAlfauzan <alfauzansepta@gmail.com>
2024-12-30 21:28:48 +0700

    update(sidebar): add sidebar for main layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,122 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final screenSize = WindowScreen().calculateScreenWidthSize(context);
-
-//    print(screenSize);
-
     return MaterialApp(
       title: 'SQLite Playground',
       theme: getDarkTheme(),
       themeMode: ThemeMode.dark,
-      home: const PlaygroundView(),
+      home: const ContainerPage(),
       debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+class ContainerPage extends StatefulWidget {
+  const ContainerPage({super.key});
+
+  @override
+  State<ContainerPage> createState() => _ContainerPageState();
+}
+
+class _ContainerPageState extends State<ContainerPage> {
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = WindowScreen().calculateScreenWidthSize(context);
+
+    return Material(
+      child: Flex(
+        direction: screenSize == ScreenWidthSize.COMPACT
+            ? Axis.vertical
+            : Axis.horizontal,
+        children: screenSize == ScreenWidthSize.COMPACT
+            ? _buildChildren(screenSize).reversed.toList()
+            : _buildChildren(screenSize),
+      ),
+    );
+  }
+
+  List<Widget> _buildChildren(ScreenWidthSize screenSize) {
+    return [
+      Container(
+        height: screenSize == ScreenWidthSize.COMPACT ? 56 : double.infinity,
+        color: Colors.black,
+        padding: const EdgeInsets.all(8),
+        child: Flex(
+          mainAxisAlignment: screenSize == ScreenWidthSize.COMPACT
+              ? MainAxisAlignment.center
+              : MainAxisAlignment.start,
+          crossAxisAlignment: screenSize == ScreenWidthSize.COMPACT
+              ? CrossAxisAlignment.center
+              : CrossAxisAlignment.start,
+          direction: screenSize == ScreenWidthSize.COMPACT
+              ? Axis.horizontal
+              : Axis.vertical,
+          children: [
+            Flex(
+              direction: screenSize == ScreenWidthSize.COMPACT
+                  ? Axis.vertical
+                  : Axis.horizontal,
+              children: [
+                Icon(Icons.code),
+                const SizedBox(
+                  width: 4,
+                  height: 4,
+                ),
+                Text(
+                  'Playground',
+                  style: TextStyle(
+                      fontSize: screenSize == ScreenWidthSize.COMPACT ? 8 : 12),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 12,
+              height: 12,
+            ),
+            Flex(
+              direction: screenSize == ScreenWidthSize.COMPACT
+                  ? Axis.vertical
+                  : Axis.horizontal,
+              children: [
+                Icon(Icons.dataset),
+                const SizedBox(
+                  width: 4,
+                  height: 4,
+                ),
+                Text(
+                  'ERD',
+                  style: TextStyle(
+                      fontSize: screenSize == ScreenWidthSize.COMPACT ? 8 : 12),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 12,
+              height: 12,
+            ),
+            Flex(
+              direction: screenSize == ScreenWidthSize.COMPACT
+                  ? Axis.vertical
+                  : Axis.horizontal,
+              children: [
+                Icon(Icons.storage_rounded),
+                const SizedBox(
+                  width: 4,
+                  height: 4,
+                ),
+                Text(
+                  'Databases',
+                  style: TextStyle(
+                      fontSize: screenSize == ScreenWidthSize.COMPACT ? 8 : 12),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      Expanded(
+        child: const PlaygroundView(),
+      ),
+    ];
   }
 }
